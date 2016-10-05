5bc0c8eefe5b455cf9be7007265fcce7711aa794
alisdt <alisdt@users.noreply.github.com>
2016-10-05 13:52:09 +0100

    M    remote_user/remote_user_auth.py - Email address -> username

--- remote_user/remote_user_auth.py
+++ remote_user/remote_user_auth.py
@@ -1,20 +1,31 @@
 
+import re
 import os,os.path
+
 from jupyterhub.handlers import BaseHandler
 from jupyterhub.auth import Authenticator
 from jupyterhub.utils import url_path_join
 from tornado import gen, web
 from traitlets import Unicode
 
+_EMAIL_RE = re.compile(r'[\w\d._%+-]+@[\w\d.-]+\.\w{2,}')
 
 class RemoteUserLoginHandler(BaseHandler):
 
+    @staticmethod
+    def email_to_user(username):
+        if _EMAIL_RE.match(username):
+            return re.sub(r'^[\w]','_',username)
+        else:
+            return username
+    
     def get(self):
         header_name = self.authenticator.header_name
         remote_user = self.request.headers.get(header_name, "")
         if remote_user == "":
             raise web.HTTPError(401)
         else:
+            remote_user = self.email_to_user(username)
             if not os.path.exists('/home/'+remote_user):
                 raise web.HTTPError(reason="directory does not exist, to gain access, please contact ithelp@geos.ed.ac.uk",status_code=403)
             user = self.user_from_username(remote_user)
